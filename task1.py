da743b5204d475a4055340925e68975b3bcb0b95
PorRahul <poralurahul63@gmail.com>
2025-08-07 20:34:39 +0530

    Basic_Math_Operation_task1.py

---
+++ task1.py
@@ -0,0 +1,22 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Cannot divide by zero"
+    return a / b
+
+if __name__ == "__main__":
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+
+    print("Addition:", add(a, b))
+    print("Subtraction:", subtract(a, b))
+    print("Multiplication:", multiply(a, b))
+    print("Division:", divide(a, b))
